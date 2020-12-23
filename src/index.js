264018e15043560c011812542064c6393ad631d3
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2020-12-23 16:26:58 +0200

    body changes colors always

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import css from "./css/styles.css";
 
+const refs = {
+  body: document.querySelector("body"),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+
 const colors = [
   "#FFFFFF",
   "#2196F3",
@@ -12,3 +18,15 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+refs.startBtn.addEventListener("click", onChangeColor());
+
+// console.dir(refs.body.style.backgroundColor);
+// console.log(colors[randomIntegerFromInterval(0, 5)]);
+
+function onChangeColor() {
+  setInterval(() => {
+    let color = colors[randomIntegerFromInterval(0, colors.length)];
+    refs.body.style.backgroundColor = color;
+  }, 1000);
+}
